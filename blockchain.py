871dce5d2484442c28dc676e9a55bcc94633f6dc
Isaac <isaachulvey@gmail.com>
2019-12-26 17:22:49 -0500

    working basic blockchain POC to-do: add ability to register new nodes/consensus algorithm

--- blockchain.py
+++ blockchain.py
@@ -8,6 +8,7 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        # Add in consensus algorithm to allow for registering new nodes
 
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
